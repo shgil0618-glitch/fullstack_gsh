f3fab7aae2762bfefe2a484f4cdc589ad0dcf7b0
gilsanghyun <shgil0618@gmail.com>
2025-09-19 12:13:48 +0900

    test

--- workspace/track003_java/java2025/src/com/company/javaex_test/TestEx020_6.java
+++
@@ -1,8 +0,0 @@
-package com.company.javaex_test;
-
-public class TestEx020_6 {
-	   public static void main(String[] args) {
-		    System.out.println("c에서 오류가 발생합니다.");
-		    System.out.println("오버로딩은 자료형과 갯수로만 판단하기 때문에!");
-		   }
-}
