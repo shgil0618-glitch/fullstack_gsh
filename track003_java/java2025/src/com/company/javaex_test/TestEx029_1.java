0b38d8817ff2fbd850e93ad2be7a7a62af88d6a8
gilsanghyun <shgil0618@gmail.com>
2025-10-02 11:57:34 +0900

    day029

---
+++ track003_java/java2025/src/com/company/javaex_test/TestEx029_1.java
@@ -0,0 +1,17 @@
+package com.company.javaex_test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestEx029_1 {
+	public static void main(String[] args) {
+		List<String> colors = new ArrayList<String>();
+		colors.add("red");
+		colors.add("green");
+		colors.add("blue");
+		
+		System.out.println(colors);
+
+	}
+
+}
