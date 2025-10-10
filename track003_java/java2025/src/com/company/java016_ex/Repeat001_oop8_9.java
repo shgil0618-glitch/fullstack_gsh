381bd22ca394fc55c6592810210d0dabf15386f3
gilsanghyun <shgil0618@gmail.com>
2025-10-10 16:42:38 +0900

    자기소개

--- track003_java/java2025/src/com/company/java016_ex/Repeat001_oop8_9.java
+++ track003_java/java2025/src/com/company/java016_ex/Repeat001_oop8_9.java
@@ -1,15 +1,10 @@
 package com.company.java016_ex;
 
 // abstract 과 interface 의 공통점과 차이점
-// 공통점 : 자손객체를 통해서 코드를 구현
+// 공통점 : 둘 다 객체의 설계를 정의하며, 인스턴스를 직접 만들 수 없음.
 // 차이점 : 추상화정도가 interface 가 더 크다 
-//		abstract  - 인스턴스변수, 인스턴스메서드 사용가능
-//      interface - 상수     , abstract 메서드
-/*
- 		 Vehicle (run)
- 		↑ 		↑
- MotorCycle     Car(run) @Override
-*/
+//		abstract는 구현 일부 포함 가능, interface는 순수 메서드 선언만 가능
+
 
 interface Vehicle{public void run();}
 class MotorCycle implements Vehicle{
@@ -17,16 +12,23 @@ class MotorCycle implements Vehicle{
 	{System.out.println("오토바이가 달립니다.");}
 	public void helmet() {System.out.println("헬멧을 착용합니다");}
 }
+
 class Car implements Vehicle{
 	@Override public void run()
 	{System.out.println("자동차가 달립니다.");}
 }
+
 class Driver{
-	void drive(Vehicle a) {
-		a.run();
-		if( a instanceof MotorCycle ) {  ((MotorCycle) a).helmet(); }
+	void drive(Vehicle v) {
+		v.run();
+		// ((MotorCycle) v).helmet();
+		if( v instanceof MotorCycle ) {  ((MotorCycle) v).helmet(); }
+		// v instanceof MotorCycle는 단순 클래스 확인인줄 알았는데 이게 없으면 출력에서 오류가 발생합니다 이유가 뭘까요...?
+		// 답변 : 위에 상속도를 보면 하나의 부모에 두개의 자식이있는데, 누구의 자식인지 찾을려고 사용. 
+		// else if로 하나 더하면 안되네. if구문 여러개로 사용
 	}
 }
+
 public class Repeat001_oop8_9 {
 
 	public static void main(String[] args) {
@@ -35,9 +37,8 @@ public class Repeat001_oop8_9 {
 		MotorCycle motorCycle = new MotorCycle();
 		Car car = new Car();
 		
-		driver.drive(car); // 리턴값 메서드명(파라미터)
-						   // void drive(car)   
-		   				   // void drive(motorCycle) 
+		driver.drive(car); 
+
 		System.out.println("\n\n>>자동차가 고장나서 교통수단을 바꿉니다");
 
 		driver.drive(motorCycle);
