bec25ed1e3e318b9fdb4631781f6686ef5f9ab4c
gilsanghyun <shgil0618@gmail.com>
2025-11-24 13:19:07 +0900

    1124

---
+++ track006_spring/spring005/src/main/java/com/thejoa703/dao/UserDao.java
@@ -0,0 +1,30 @@
+package com.thejoa703.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.thejoa703.dto.UserDto;
+
+@MyDao
+public interface UserDao {
+
+    public int insert(UserDto dto);
+    public int update(UserDto dto);
+    public int delete(UserDto dto);
+    public List<UserDto> selectAll();
+    public UserDto select(int appUserId);
+    public UserDto login(UserDto dto); // 이메일, 비밀번호로 로그인 처리
+    
+    //이미지 추가
+    public int insert2(UserDto dto);
+    public int update2(UserDto dto);
+    
+	/* iddoutble */
+    public int iddouble(String email);
+    
+    /*관리자 삭제 조회*/
+    public int deleteAdmin(UserDto dto);
+    public int updateAdmin(UserDto dto);
+    
+}
--- track006_spring/spring005/src/main/java/com/thejoa703/dto/EmpDto.java
+++ track006_spring/spring005/src/main/java/com/thejoa703/dto/EmpDto.java
@@ -1,18 +1,20 @@
 package com.thejoa703.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 
 @Data
 public class EmpDto {
-	private int EMPNO;
-	private String ENAME;
-	private String JOB;
-	private int MGR;
-	private String HIREDATE;
-	private int SAL;
-	private int COMM;
-	private int DEPTNO;
+	private int empno;
+	private String ename;
+	private String job;
+	private int mgr;
+	private String hiredate;
+	private int sal;
+	private int comm;
+	private int deptno;
 }
 
 
