6fae68a540fc54cdfd3ddc8af8ed04523ec66337
gilsanghyun <shgil0618@gmail.com>
2026-01-06 09:49:06 +0900

    0106

--- track008_nodereact/basic1_node_react/back/config/db.js
+++ track008_nodereact/basic1_node_react/back/config/db.js
@@ -3,6 +3,6 @@
 require('dotenv').config();
 module.exports = {
     user: process.env.DB_USER,
-    passoword: process.env.DB_PASSWORD,
+    password: process.env.DB_PASSWORD,
     connectString: process.env.DB_CONNECT   //localhost:1521/XE
 };
